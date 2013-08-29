8b6af509f3834ec9b670db3119143e9d6506ca14
Daniel Friesen <daniel@nadir-seen-fire.com>
2013-08-29 02:06:28 -0700

    Update extension metadata. - License blurb - Author lines - Other attributes
    Change-Id: I6b6f7aa1fcff80a0b6dff128d1d29fa335a9136f

--- NamespacePaths.php
+++ NamespacePaths.php
@@ -1,25 +1,13 @@
 <?php
 /**
- * Copyright 2010 Daniel Friesen
+ * Namespace Paths
  *
  * @file
  * @ingroup Extensions
- * @author Daniel Friesen (http://mediawiki.org/wiki/User:Dantman) <mediawiki@danielfriesen.name>
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * @author Daniel Friesen (http://danf.ca/mw/)
+ * @copyright Copyright © 2010 – Daniel Friesen
+ * @license https://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @link https://www.mediawiki.org/wiki/Extension:NamespacePaths Documentation
  */
 
 if ( !defined( 'MEDIAWIKI' ) ) die( "This is an extension to the MediaWiki package and cannot be run standalone." );
@@ -28,7 +16,7 @@ $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'NamespacePaths',
 	'version' => '1.0',
-	'author' => array( '[http://mediawiki.org/wiki/User:Dantman Daniel Friesen]', '[http://redwerks.org/mediawiki/ Redwerks]' ),
+	'author' => array( '[http://danf.ca/mw/ Daniel Friesen]', '[http://redwerks.org/mediawiki/ Redwerks]' ),
 	'url' => 'https://www.mediawiki.org/wiki/Extension:NamespacePaths',
 	'descriptionmsg' => 'namespacepaths-desc',
 );
